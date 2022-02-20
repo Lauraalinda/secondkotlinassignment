228f5e46120a327a25f8dc45e85374b5e3296281
Lauraalinda <lauraallindaa@gmail.com>
2022-02-20 21:48:58 +0300

    my second kotlin assignment

---
+++ src/loloz.kt
@@ -0,0 +1,31 @@
+fun main () {
+     printHello()
+     modulus()
+     sum()
+     factaboutmyself()
+}
+
+fun printHello(){
+    println("Hello laura")
+
+}
+
+fun modulus() {
+    var number1=20
+    var number2=3
+    println(number1%number2)
+}
+
+fun sum(){
+    var number3=30
+    var number4=40
+    var number5=50
+    var number6=60
+    var sum=(number3+number4+number5+number6)
+    println(sum)
+}
+
+fun factaboutmyself(){
+    var statement="I am good at art"
+    println(statement)
+}
